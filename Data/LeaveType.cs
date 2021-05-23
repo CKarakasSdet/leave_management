df2c228c36d92758056d5bba99277dbf1635c8ff
jack karakas <ckarakassdet@gmail.com>
2021-05-23 13:05:13 -0500

    initial commit

---
+++ Data/LeaveType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace leave_management.Data
+{
+    public class LeaveType
+    {
+        public LeaveType()
+        { }
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+
+}
