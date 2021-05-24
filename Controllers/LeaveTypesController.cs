a350656959e0f9d1e29f1d5a723a92aeb2f3af59
jack karakas <ckarakassdet@gmail.com>
2021-05-24 08:56:14 -0500

    set up details view

--- Controllers/LeaveTypesController.cs
+++ Controllers/LeaveTypesController.cs
@@ -33,7 +33,14 @@ namespace leave_management.Controllers
         // GET: LeaveTypes/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            if (!_repo.IsPresent(id))
+            {
+                return NotFound(); 
+            }
+
+            var leavetype = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeVM>(leavetype); 
+            return View(model);
         }
 
         // GET: LeaveTypes/Create
